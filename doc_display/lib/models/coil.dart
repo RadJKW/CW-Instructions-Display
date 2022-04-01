19060127729893ba3dd7cc6c4264a4a44bf6a153
Jared West <jwest@howard.com>
2022-04-01 15:20:16 -0500

    actual 'working' coil manager

--- doc_display/lib/models/coil.dart
+++ doc_display/lib/models/coil.dart
@@ -8,60 +8,45 @@ class Coil {
 
   // create a variable to hold the mqtt received message string
   String? receivedMessage;
-  final String _number;
-  final String _division;
-  final String _winding;
-  final String _material;
-  final String _materialWidth;
-  final String _prevStop;
-  final String _activeStop;
-  final String _nextStop;
+  final String number;
+  final String division;
+  final String winding;
+  final String material;
+  final String materialWidth;
+  final String prevStop;
+  final String activeStop;
+  final String nextStop;
 
-  Coil({
-      required String number,
-      required String division,
-      required String winding,
-      required String material,
-      required String materialWidth,
-      required String prevStop,
-      required String activeStop,
-      required String nextStop}) : _number = number,
-            _division = division,
-            _winding = winding,
-            _material = material,
-            _materialWidth = materialWidth,
-            _prevStop = prevStop,
-            _activeStop = activeStop,
-            _nextStop = nextStop;
+  Coil(this.number, this.division, this.winding, this.material,
+      this.materialWidth, this.prevStop, this.activeStop, this.nextStop);
 
   Coil.fromJson(Map<String, dynamic> json)
-      : _number = json['number'],
-        _division = json['division'],
-        _winding = json['winding'],
-        _material = json['material'],
-        _materialWidth = json['materialWidth'],
-        _prevStop = json['prevStop'],
-        _activeStop = json['activeStop'],
-        _nextStop = json['nextStop'];
+      : number = json['number'],
+        division = json['division'],
+        winding = json['winding'],
+        material = json['material'],
+        materialWidth = json['materialWidth'],
+        prevStop = json['prevStop'],
+        activeStop = json['activeStop'],
+        nextStop = json['nextStop'];
 
   Map<String, dynamic> toJson() => {
-        'number': _number,
-        'division': _division,
-        'winding': _winding,
-        'material': _material,
-        'materialWidth': _materialWidth,
-        'prevStop': _prevStop,
-        'activeStop': _activeStop,
-        'nextStop': _nextStop,
+        'number': number,
+        'division': division,
+        'winding': winding,
+        'material': material,
+        'materialWidth': materialWidth,
+        'prevStop': prevStop,
+        'activeStop': activeStop,
+        'nextStop': nextStop,
       };
 
-  String get getNumber => _number;
-  String get getDivision => _division;
-  String get getWinding => _winding;
-  String get getMaterial => _material;
-  String get getMaterialWidth => _materialWidth;
-  String get getPrevStop => _prevStop;
-  String get getActiveStop => _activeStop;
-  String get getNextStop => _nextStop;
-  
+  String get getNumber => number;
+  String get getDivision => division;
+  String get getWinding => winding;
+  String get getMaterial => material;
+  String get getMaterialWidth => materialWidth;
+  String get getPrevStop => prevStop;
+  String get getActiveStop => activeStop;
+  String get getNextStop => nextStop;
 }
