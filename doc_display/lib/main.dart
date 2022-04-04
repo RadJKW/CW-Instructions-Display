28ee62c5ea46a2ff6d7b21f91ff913936babe749
Jared West <jwest@howard.com>
2022-04-04 16:07:25 -0500

    moved mqtt provider downstream

--- doc_display/lib/main.dart
+++ doc_display/lib/main.dart
@@ -36,11 +36,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AppTheme()),
-        ChangeNotifierProvider(create: (_) => MqttAppState()),
-      ],
+    return ChangeNotifierProvider(
+      create: (_) => AppTheme(),
       builder: (context, _) {
         final appTheme = context.watch<AppTheme>();
         return FluentApp(
@@ -66,12 +63,6 @@ class MyApp extends StatelessWidget {
               glowFactor: is10footScreen() ? 2.0 : 0.0,
             ),
           ),
-          builder: (context, child) {
-            return Directionality(
-              textDirection: appTheme.textDirection,
-              child: child!,
-            );
-          },
         );
       },
     );
@@ -87,8 +78,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> with WindowListener {
   bool value = false;
-
-  int index = 4;
 
   final settingsController = ScrollController();
 
@@ -108,106 +97,86 @@ class _MyHomePageState extends State<MyHomePage> with WindowListener {
   @override
   Widget build(BuildContext context) {
     final appTheme = context.watch<AppTheme>();
-    return NavigationView(
-      appBar: NavigationAppBar(
-        title: () {
-          if (kIsWeb) return const Text(appTitle);
-          return const DragToMoveArea(
-            child: Align(
-              alignment: AlignmentDirectional.centerStart,
-              child: Text(appTitle),
-            ),
-          );
-        }(),
-        actions: kIsWeb
-            ? null
-            : DragToMoveArea(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [Spacer()],
+    return ChangeNotifierProvider(
+      create: (_) => MqttAppState(),
+      builder: (context, _) {
+        final mqttAppState = context.watch<MqttAppState>();
+        int index = mqttAppState.getCurrentPage;
+        return NavigationView(
+          appBar: NavigationAppBar(
+            title: () {
+              return const DragToMoveArea(
+                child: Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text(appTitle),
                 ),
+              );
+            }(),
+            actions: DragToMoveArea(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [Spacer()],
               ),
-      ),
-      pane: NavigationPane(
-        selected: index,
-        onChanged: (i) => setState(() => index = i),
-        size: const NavigationPaneSize(
-          openMinWidth: 250,
-          openMaxWidth: 320,
-        ),
-        header: Container(
-          height: kOneLineTileHeight,
-          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-          child: const FlutterLogo(
-            style: FlutterLogoStyle.horizontal,
-            size: 100,
+            ),
           ),
-        ),
-        displayMode: appTheme.displayMode,
-        indicatorBuilder: () {
-          switch (appTheme.indicator) {
-            case NavigationIndicators.end:
-              return NavigationIndicator.end;
-            case NavigationIndicators.sticky:
-            default:
-              return NavigationIndicator.sticky;
-          }
-        }(),
-        items: [
-          // TODO: Instead of populating these manually,
-          //  use a list or map type thing.
-          PaneItem(
-            // index 0 - dashboard.dart
-            icon: const Icon(FluentIcons.view_dashboard),
-            title: const Text('    Dashboard'),
+          pane: NavigationPane(
+            selected: index,
+            onChanged: (i) => mqttAppState.setCurrentPage(i),
+            // (i) => setState(() => index = i),
+            size: const NavigationPaneSize(
+              openMinWidth: 250,
+              openMaxWidth: 320,
+            ),
+            header: Container(
+              height: kOneLineTileHeight, // kOneLinetileHeight = 40
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: const FlutterLogo(
+                style: FlutterLogoStyle.horizontal,
+                size: 100,
+              ),
+            ),
+            displayMode: appTheme.displayMode,
+            indicatorBuilder: NavigationIndicator.end,
+            items: [
+              PaneItem(
+                  // index 0
+                  icon: const Icon(FluentIcons.view_dashboard),
+                  title: const Text('    Dashboard')),
+              PaneItem(
+                  // index 1
+                  icon: const Icon(FluentIcons.folder_list),
+                  title: const Text('    Browse')),
+              PaneItem(
+                  // index 2
+                  icon: const Icon(FluentIcons.pdf),
+                  title: const Text('    PDF Viewer')),
+              PaneItem(
+                  // index 3
+                  icon: const Icon(FluentIcons.photo_video_media),
+                  title: const Text('    Videos')),
+              PaneItem(
+                  // index 4
+                  icon: const Icon(FluentIcons.network_tower),
+                  title: const Text('    MQTT Test')),
+            ],
+            footerItems: [
+              PaneItemSeparator(),
+              PaneItem(
+                icon: const Icon(FluentIcons.settings),
+                title: const Text('Settings'),
+              ),
+            ],
           ),
-          PaneItem(
-            // index 1 - browse.dart
-            icon: const Icon(FluentIcons.folder_list),
-            title: const Text('    Browse'),
-          ),
-          PaneItem(
-            // index 2
-            icon: const Icon(FluentIcons.pdf),
-            title: const Text('    PDF Viewer'),
-          ),
-          PaneItem(
-            // index 3
-            icon: const Icon(FluentIcons.photo_video_media),
-            title: const Text('    Videos'),
-          ),
-          PaneItem(
-            // index 4
-            icon: const Icon(FluentIcons.network_tower),
-            title: const Text('    MQTT Test'),
-          ),
-        ],
-        autoSuggestBox: AutoSuggestBox(
-          controller: TextEditingController(),
-          items: const ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
-        ),
-        autoSuggestBoxReplacement: const Icon(FluentIcons.search),
-        footerItems: [
-          PaneItemSeparator(),
-          PaneItem(
-            icon: const Icon(FluentIcons.settings),
-            title: const Text('Settings'),
-          ),
-        ],
-      ),
-      content: NavigationBody(index: index, children: [
-        const Dashboard(), // 0
-
-        const BrowsePage(), // 1
-
-        const PdfPage(), // 2
-
-        const VidplayerPage(), // 3
-
-        const MqttView(), // 4
-
-        SettingsPage(controller: settingsController),
-      ]),
+          content: NavigationBody(index: index, children: [
+            const Dashboard(), // 0
+            const BrowsePage(), // 1
+            const PdfPage(), // 2
+            const VidplayerPage(), // 3
+            const MqttView(), // 4
+            SettingsPage(controller: settingsController),
+          ]),
+        );
+      },
     );
   }
 }
